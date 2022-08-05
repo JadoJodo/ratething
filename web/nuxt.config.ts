ee50b9af7defb87d44f8e9c3f7300105f3b3b7f3
Jordan Finnigan <light.lion8513@hxl.io>
2022-08-05 12:19:43 +0000

    Add Staged files

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -5,5 +5,11 @@ export default defineNuxtConfig({
     ssr: false,
     app: {
         baseURL: "/app/"
-    }
+    },
+    devServer: {
+        allowedHosts: 'all',
+        client: {
+            webSocketURL: 'auto://0.0.0.0:0/ws'
+        }
+    },
 })
